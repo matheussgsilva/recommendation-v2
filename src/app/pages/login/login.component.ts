6a359801fc2039a1684db3114be91160bdaa7152
Matheussgsilva <matheussgsilva@hotmail.com>
2022-10-27 10:39:28 -0300

    Create form group in Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  public form: FormGroup = new FormGroup({
+    email: new FormControl('joao.scarpa@designa.com', [Validators.required]),
+    password: new FormControl('123456', [Validators.required])
+  })
 
 }
