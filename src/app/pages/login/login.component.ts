f77b936e0df15419aaffbf3abcd6a19ed93ccd58
Matheussgsilva <matheussgsilva@hotmail.com>
2022-10-27 10:52:44 -0300

    Create function login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,8 @@
+import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +11,24 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent {
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService,
+    private authService: AuthService
+  ) { }
 
   public form: FormGroup = new FormGroup({
     email: new FormControl('joao.scarpa@designa.com', [Validators.required]),
     password: new FormControl('123456', [Validators.required])
   })
+
+  public login(): void {
+    if(this.form.valid) {
+      this.apiService.post<User>('sessions', this.form.value)
+      .then(data => {this.authService.login(data)})
+      .catch(response => console.log(response.error.error))
+    } else {
+      console.log("Preencha o formulário antes de continuar")
+    }
+  }
 
 }
