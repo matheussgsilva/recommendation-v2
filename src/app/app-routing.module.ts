8e94d481e93924747d230c23c57acce5cccf21f2
Matheussgsilva <matheussgsilva@hotmail.com>
2022-10-29 14:45:30 -0300

    Add canActivate in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { RecommendationListComponent } from './pages/recommendation-list/recommendation-list.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registre-se', component: RegisterComponent },
-  { path: '', component: LayoutComponent, children:[
+  { path: '', component: LayoutComponent, canActivate: [ AuthGuard ], children:[
     { path: '', component: RecommendationListComponent }
   ]}
 ];
